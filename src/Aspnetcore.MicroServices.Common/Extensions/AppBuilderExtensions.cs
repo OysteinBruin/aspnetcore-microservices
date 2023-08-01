b569c703512f74e355f105240c501df67afba85a
Øystein Hoveland Bruin <oystein.bruin@epinova.no>
2023-08-01 08:11:23 +0200

    Rename and restructure solution, add AppBuilderExtension

---
+++ src/Aspnetcore.MicroServices.Common/Extensions/AppBuilderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspnetcore.MicroServices.Common.Extensions
+{
+    public static class AppBuilderExtensions
+    {
+    }
+}
