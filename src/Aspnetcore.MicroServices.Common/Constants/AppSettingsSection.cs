723e5cd1fccf0535f9a3f0950ed5d89b324fe230
Øystein Hoveland Bruin <oystein.bruin@epinova.no>
2023-08-01 10:16:06 +0200

    Update AppBuilderExtensions

---
+++ src/Aspnetcore.MicroServices.Common/Constants/AppSettingsSection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspnetcore.MicroServices.Common.Constants
+{
+    internal static class AppSettingsSection
+    {
+        public const string Identity = "Identity";
+        public const string OpenApi = "OpenApi";
+        public const string Auth = "Auth";
+        public const string KayVault = "KayVault";
+        public const string ApplicationInsights = "ApplicationInsights";
+        public const string EventBus = "EventBus";
+    }
+}
