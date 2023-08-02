5823a638eb53738359d98e33e024e8adb81fa972
Øystein Hoveland Bruin <oystein.bruin@epinova.no>
2023-08-02 09:49:08 +0200

    Update Nuke build, add appveyor,yml

--- build/Build.cs
+++ build/Build.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Linq;
+
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using Nuke.Common.Git;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+
 
 class Build : NukeBuild
 {
@@ -23,6 +20,17 @@ class Build : NukeBuild
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    [Parameter] 
+    string NugetApiUrl = "https://api.nuget.org/v3/index.json"; //default
+    [Parameter] 
+    string NugetApiKey;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath ProjectPath => RootDirectory / "src" / "Aspnetcore.MicroServices.Common" / "Aspnetcore.MicroServices.Common.csproj";
 
     Target Clean => _ => _
         .Before(Restore)
@@ -41,4 +49,39 @@ class Build : NukeBuild
         {
         });
 
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPack(s => s
+                .SetProject(ProjectPath)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .EnableNoRestore()
+                .SetDescription("Sample package produced by NUKE")
+                .SetPackageTags("nuke demonstration c# library")
+                .SetNoDependencies(true)
+                .SetOutputDirectory(ArtifactsDirectory / "nuget"));
+
+        });
+    
+    /*Target Push => _ => _
+        .DependsOn(Pack)
+        .Requires(() => NugetApiUrl)
+        .Requires(() => NugetApiKey)
+        .Requires(() => Configuration.Equals(Configuration.Release))
+        .Executes(() =>
+        {
+            GlobFiles(NugetDirectory, "*.nupkg")
+                .NotEmpty()
+                .Where(x => !x.EndsWith("symbols.nupkg"))
+                .ForEach(x =>
+                {
+                    DotNetNuGetPush(s => s
+                        .SetTargetPath(x)
+                        .SetSource(NugetApiUrl)
+                        .SetApiKey(NugetApiKey)
+                    );
+                });
+        });*/
 }
